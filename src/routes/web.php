9fd0f90ce27385d2d34d16dc978b8a179fd93b58
ConsumingToast <teodoradiaconu99@gmail.com>
2021-09-12 12:27:26 +0300

    post logic

---
+++ src/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
+
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/posts', [PostController::class, 'store']);
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
